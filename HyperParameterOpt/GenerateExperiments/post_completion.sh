f120f493d25732c93b9e3c97f214bdedd08b47c8
Joseph Wilkes <42664702+jbwilkes@users.noreply.github.com>
2020-05-29 10:42:26 -0600

    Update post_completion.sh

--- HyperParameterOpt/GenerateExperiments/post_completion.sh
+++ HyperParameterOpt/GenerateExperiments/post_completion.sh
@@ -22,7 +22,7 @@ mv #DIR#/#FNAME#*.pkl #DIR#/#FNAME#/result_files
 
 cd #DIR#
 # the data can easily, automatically be moved to a folder in a file sharing location.
-# mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/jw_data/jw27+
+# mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/jw_data/jw38+
 # mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/dj_data
 # mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/jj_data
 mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/unsorted_data
