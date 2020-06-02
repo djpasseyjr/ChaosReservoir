fdf89d208673b5a42570590c7937574e64eafb64
Joseph Wilkes <42664702+jbwilkes@users.noreply.github.com>
2020-06-02 11:53:41 -0600

    move to compute directory, more storage, not backedup

--- HyperParameterOpt/GenerateExperiments/post_completion.sh
+++ HyperParameterOpt/GenerateExperiments/post_completion.sh
@@ -22,10 +22,10 @@ mv #DIR#/#FNAME#*.pkl #DIR#/#FNAME#/result_files
 
 cd #DIR#
 # the data can easily, automatically be moved to a folder in a file sharing location.
-# mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/jw_data/jw38+
+# mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/compute/jw_data/jw38+
 # mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/dj_data
 # mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/jj_data
-mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/unsorted_data
+mv #FNAME#/ ~/fsl_groups/fslg_webb_reservoir/compute/unsorted_data
 # mv #FNAME#/ ~/compute/Saved_data/
 # mv #FNAME#/ ~/compute
 cd ..
