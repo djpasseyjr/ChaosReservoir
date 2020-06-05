4158f03143ac762f3ac14eed1deb4e2393facc09
DJ Passey <djpasseyjr@gmail.com>
2020-06-05 11:23:27 -0600

    Track experiment number, function to merge compiled dicts

--- HyperParameterOpt/GenerateExperiments/compile_dicts.py
+++ HyperParameterOpt/GenerateExperiments/compile_dicts.py
@@ -1,4 +1,4 @@
-#compile_dicts.py 
+#compile_dicts.py
 import numpy as np
 import pickle
 import pandas as pd
@@ -59,35 +59,39 @@ def compile_output(DIR, filename_prefix, num_experiments, nets_per_experiment):
         # Load next data dictionary
         try:
             data_dict = pickle.load(open(path + str(i) + '.pkl','rb'))
+            # Add data to compiled dictionary
             add_to_compiled(compiled, data_dict, start_idx)
         except:
             failed_file_count += 1
-        # Add data to compiled dictionary
+        # Track experiment number
+        for k in range(start_idx, start_idx + nets_per_experiment):
+            data_dict["exp_num"][k] = i
+
         start_idx += nets_per_experiment
-        
+
         if verbose:
             if i % 1000 == 0:
-                info = f'\n{i} files compile attempted,time since start (minutes):{round((time.time() - start )/ 60,1)}' 
-                timing += info                
+                info = f'\n{i} files compile attempted,time since start (minutes):{round((time.time() - start )/ 60,1)}'
+                timing += info
                 print(info)
     #write final dict to pkl file
     pickle.dump(compiled, open('compiled_output_' + filename_prefix + '.pkl', 'wb'))
-    
+
     if verbose:
         # Time difference is originally seconds
         finished = (time.time() - start )/ 60
         info = f'\nit took {round(finished,1)} minutes to compile\nor {round(finished / 60,1)} hours'
-        file += info 
+        file += info
         print(info)
         info = f'\n(#failed files) / (# total number of experiments) is {failed_file_count} / {NEXPERIMENTS}\nor {100 * round(failed_file_count/NEXPERIMENTS, 1)}% failed'
-        file += info 
+        file += info
         print(info)
         ending = f'\n{filename_prefix} compilation process finished'
-        timing += ending 
+        timing += ending
         #only write to the file once, the file will close automatically
         with open(f'{filename_prefix}_compiling_notes.txt','w') as f:
             f.write(file + timing)
-        
+
 
 def empty_result_dict(num_experiments, nets_per_experiment):
     """ Make empty dictionary for compiling data """
@@ -99,6 +103,7 @@ def empty_result_dict(num_experiments, nets_per_experiment):
         empty[colname] = [[]] * nentries
     for colname in STRING_COLNAMES:
         empty[colname] = [''] * nentries
+    empty["exp_num"] = [-1] * nentries
     return empty
 
 def add_to_compiled(compiled, data_dict, start_idx):
@@ -106,5 +111,16 @@ def add_to_compiled(compiled, data_dict, start_idx):
     for k in data_dict.keys():
         for colname in FLOAT_COLNAMES + STRING_COLNAMES + LIST_COLNAMES:
             compiled[colname][start_idx + k] = data_dict[k][colname]
+
+def merge_compiled(compiled1, compiled2):
+    """ Merge two compiled dictionaries """
+    if isinstance(compiled1, str) and isinstance(compiled2, str):
+        compiled1 = pickle.load(open(compiled1, 'rb'))
+        compiled2 = pickle.load(open(compiled2, 'rb'))
+    for k in compiled1.keys():
+        compiled1[k] += compiled2[k]
+    return compiled1
+
+
 
 compile_output(DIR,filename_prefix,NEXPERIMENTS,NETS_PER_EXPERIMENT)
