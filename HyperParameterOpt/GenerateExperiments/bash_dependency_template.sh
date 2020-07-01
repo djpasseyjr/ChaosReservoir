456d4a818a7a25e14abc1739c7e17ad099a94eda
Joseph Wilkes <joseph.wilkes@byu.edu>
2020-07-01 14:30:20 -0600

    changes

--- HyperParameterOpt/GenerateExperiments/bash_dependency_template.sh
+++ HyperParameterOpt/GenerateExperiments/bash_dependency_template.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+#create a copy of the main
+cp main.py #FILENAME_PREFIX#_main.py
+echo "copied main.py to #FILENAME_PREFIX#_main.py"
+
 echo "starting --dependency=singleton --job-name=#JOB_NAME#"
-echo "\nstarting --dependency=singleton --job-name=#JOB_NAME#" >> my_saved_ids.txt
+echo "starting --dependency=singleton --job-name=#JOB_NAME#" >> my_saved_ids.txt
 echo "$(date)" >> my_saved_ids.txt
 echo "sbatch #FILENAME_PREFIX#.sh" >> my_saved_ids.txt
 sbatch --dependency=singleton --job-name=#JOB_NAME# #FILENAME_PREFIX#.sh >> my_saved_ids.txt
@@ -9,10 +14,3 @@ sbatch --dependency=singleton --job-name=#JOB_NAME# individual_partition_compila
 echo "all_partitions_compilation_#FILENAME_PREFIX#.sh" >> my_saved_ids.txt
 sbatch --dependency=singleton --job-name=#JOB_NAME# all_partitions_compilation_#FILENAME_PREFIX#.sh >> my_saved_ids.txt
 echo "finished" >> my_saved_ids.txt
-
-#how to include cleanup, and final.sh
-# move the save_ids.txt file to a directory
-# doing this with python could potentially write the job_id of the generation batch to the cleanup slurm
-# start simple then amp it up
-
-#or just have one file, where I add the date and time, and just append perpetually
