3bc7255268ae83c0ea38b96964726c5368d30282
DJ Passey <djpasseyjr@gmail.com>
2020-08-04 12:09:07 -0400

    Bug fix

--- HyperParameterOpt/GenerateExperiments/compile_dicts_template.py
+++ HyperParameterOpt/GenerateExperiments/compile_dicts_template.py
@@ -179,6 +179,7 @@ def add_net_stats(compiled, data_dict, start_idx):
         compiled["assort"][start_idx + k] = assort(g)
         compiled["cluster"][start_idx + k] = nx.average_clustering(g)
         compiled["diam"][start_idx + k] = diam
+        A = A.tocsr()
         if np.max(A) - np.min(A) < 1e-8:
             # If the edge weights aren't uniform edge weight is set to none
             compiled["edge_weight"][start_idx + k] = np.max(A)
